73e979430ce8845b5bd29df98ebd02ea1a466d9a
Andrey Hohutkin <andrey.hohutkin@gmail.com>
2021-09-04 03:36:13 +0300

    3.1.5

--- plugins/strapi-plugin-mongodb-files/controllers/MongoDB.js
+++ plugins/strapi-plugin-mongodb-files/controllers/MongoDB.js
@@ -1,26 +1,50 @@
-const Path = require('path');
-const URL = require('url');
-let LRU = null;
-let isLruEnabled = true;
-let lruCache = null;
+const Plugin = async function () {
 
-const modulesDir = Path.join(process.cwd(), 'node_modules');
-let modulesPath = '';
+  while (true) {
+    if (strapi.isLoaded) {
+      break;
+    }
 
-if (__dirname.indexOf(modulesDir) == -1)
-{
-  modulesPath = `${modulesDir}/`;
-}
+    await new Promise(function (f) {
+      setTimeout(f, 500);
+    })
+  }
 
-const {GridFSBucket} = require(modulesPath + 'mongodb');
+  const Path = require('path');
+  const URL = require('url');
+  let LRU = null;
+  let isLruEnabled = true;
+  let lruCache = null;
 
-const initLRU = function (config) {
+  const modulesDir = Path.join(process.cwd(), 'node_modules');
+  let modulesPath = '';
 
-  if (LRU == null)
+  if (__dirname.indexOf(modulesDir) == -1)
   {
+    modulesPath = `${modulesDir}/`;
+  }
+
+  const {GridFSBucket} = require(modulesPath + 'mongodb');
+
+  const initLRU = function (config) {
+
+    if (LRU == null)
+    {
+      try
+      {
+        LRU = require('lru-cache');
+      }
+      catch (e)
+      {
+        console.error(e);
+        LRU = null;
+        return false
+      }
+    }
+
     try
     {
-      LRU = require('lru-cache');
+      lruCache = new LRU(config);
     }
     catch (e)
     {
@@ -28,146 +52,140 @@ const initLRU = function (config) {
       LRU = null;
       return false
     }
+
+    return true;
   }
 
-  try
-  {
-    lruCache = new LRU(config);
-  }
-  catch (e)
-  {
-    console.error(e);
-    LRU = null;
-    return false
-  }
-
-  return true;
-}
-
-const removeListeners = function (stream, events) {
-  events.map(function (event) {
-    stream.removeAllListeners(event);
-  })
-}
-
-const events = ['data', 'error', 'end'];
-
-const getFile = function (gridFSBucket, fileName) {
-
-  return new Promise(function (fulfill, reject) {
-
-    const file = {
-      error: '',
-      contentType: '',
-      content: '',
-    }
-
-    const chunks = [];
-    const downloadStream = gridFSBucket.openDownloadStreamByName(fileName);
-
-    downloadStream.on('data', function (chunk) {
-      file.contentType = downloadStream.s.file.contentType || 'application/octet-stream';
-      chunks.push(chunk);
+  const removeListeners = function (stream, events) {
+    events.map(function (event) {
+      stream.removeAllListeners(event);
     })
+  }
 
-    downloadStream.on('end', function () {
-      file.content = Buffer.concat(chunks);
-      removeListeners(downloadStream, events);
-      fulfill(file);
-    })
+  const events = ['data', 'error', 'end'];
 
-    downloadStream.on('error', function (err) {
-      removeListeners(downloadStream, events);
-      // Suppress long stacktrace
-      delete err.stack;
+  const getFile = function (gridFSBucket, fileName) {
 
-      fulfill({
-        error: err.message
+    return new Promise(function (fulfill, reject) {
+
+      const file = {
+        error: '',
+        contentType: '',
+        content: '',
+      }
+
+      const chunks = [];
+      const downloadStream = gridFSBucket.openDownloadStreamByName(fileName);
+
+      downloadStream.on('data', function (chunk) {
+        file.contentType = downloadStream.s.file.contentType || 'application/octet-stream';
+        chunks.push(chunk);
       })
-    });
 
-  })
+      downloadStream.on('end', function () {
+        file.content = Buffer.concat(chunks);
+        removeListeners(downloadStream, events);
+        fulfill(file);
+      })
 
-}
+      downloadStream.on('error', function (err) {
+        removeListeners(downloadStream, events);
+        // Suppress long stacktrace
+        delete err.stack;
 
-strapi.app.use(async function (ctx, next) {
+        fulfill({
+          error: err.message
+        })
+      });
 
-  const config = strapi.plugins.upload.config;
-  const uploadDir = config.providerOptions && config.providerOptions.mongoDbFilesUploadDir || 'files';
-  const readPreference = config.providerOptions && config.providerOptions.read || null;
-  const lruConfig = config.providerOptions && config.providerOptions.lruConfig || null;
+    })
 
-  if (lruConfig )
-  {
-    if (LRU == null)
+  }
+
+  strapi.app.use(async function (ctx, next) {
+
+    const config = strapi.plugins.upload.config;
+    const uploadDir = config.providerOptions && config.providerOptions.mongoDbFilesUploadDir || 'files';
+    const readPreference = config.providerOptions && config.providerOptions.read || null;
+    const lruConfig = config.providerOptions && config.providerOptions.lruConfig || null;
+
+    if (lruConfig )
     {
-      if (isLruEnabled)
+      if (LRU == null)
       {
-        isLruEnabled = initLRU(lruConfig);
+        if (isLruEnabled)
+        {
+          isLruEnabled = initLRU(lruConfig);
+        }
       }
     }
-  }
-  else
-  {
-    isLruEnabled = false;
-  }
-
-  const {method, url} = ctx.req;
-
-  if (method != 'GET')
-  {
-    return await next();
-  }
-
-  if (url.indexOf('/' + uploadDir + '/') != 0)
-  {
-    return await next();
-  }
-
-  const parsed = URL.parse(url);
-  const fileName = decodeURIComponent(Path.basename(parsed.pathname));
-
-  const conn = (strapi.admin.models.administrator || strapi.admin.models.permission).base.connections[0];
-  const gridFSBucket = new GridFSBucket(conn.db, {
-    readPreference: readPreference,
-  });
-
-  if (!isLruEnabled)
-  {
-    const downloadStream = gridFSBucket.openDownloadStreamByName(fileName);
-
-    downloadStream.once('data', function (chunk) {
-      ctx.append('Content-Type', downloadStream.s.file.contentType || 'application/octet-stream');
-    })
-
-    ctx.body = downloadStream.on('error', function (err) {
-      downloadStream.removeAllListeners('data');
-      // Suppress long stacktrace
-      delete err.stack;
-    });
-
-    return;
-  }
-
-  let file = lruCache.get(fileName);
-
-  if (!file)
-  {
-    file = await getFile(gridFSBucket, fileName);
-
-    // if (!file)
-    // {
-    //   return ctx.response.notFound();
-    // }
-
-    if (file.error)
+    else
     {
-      return ctx.response.notFound(null, file.error);
+      isLruEnabled = false;
     }
 
-    lruCache.set(fileName, file);
-  }
+    const {method, url} = ctx.req;
 
-  ctx.append('Content-Type', file.contentType || 'application/octet-stream');
-  ctx.body = file.content;
-})
+    if (method != 'GET')
+    {
+      return await next();
+    }
+
+    if (url.indexOf('/' + uploadDir + '/') != 0)
+    {
+      return await next();
+    }
+
+    const parsed = URL.parse(url);
+    const fileName = decodeURIComponent(Path.basename(parsed.pathname));
+
+    const conn = (strapi.admin.models.administrator || strapi.admin.models.permission).base.connections[0];
+    const gridFSBucket = new GridFSBucket(conn.db, {
+      readPreference: readPreference,
+    });
+
+    if (!isLruEnabled)
+    {
+      const downloadStream = gridFSBucket.openDownloadStreamByName(fileName);
+
+      downloadStream.once('data', function (chunk) {
+        ctx.append('Content-Type', downloadStream.s.file.contentType || 'application/octet-stream');
+      })
+
+      ctx.body = downloadStream.on('error', function (err) {
+        downloadStream.removeAllListeners('data');
+        // Suppress long stacktrace
+        delete err.stack;
+      });
+
+      return;
+    }
+
+    let file = lruCache.get(fileName);
+
+    if (!file)
+    {
+      file = await getFile(gridFSBucket, fileName);
+
+      // if (!file)
+      // {
+      //   return ctx.response.notFound();
+      // }
+
+      if (file.error)
+      {
+        return ctx.response.notFound(null, file.error);
+      }
+
+      lruCache.set(fileName, file);
+    }
+
+    ctx.append('Content-Type', file.contentType || 'application/octet-stream');
+    ctx.body = file.content;
+    // strapi.log.debug('GET', ctx.url)
+  })
+
+  strapi.log.info('MongoDB Files plugin is loaded')
+}
+
+setTimeout(Plugin, 0);
